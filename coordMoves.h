c64df977d588160d9d5629f96c807ca29375cd34
Joseph Briggs <joseph.briggs@live.co.uk>
2021-09-18 13:28:11 +0100

    messing around with typedef

--- coordMoves.h
+++ coordMoves.h
@@ -5,10 +5,13 @@
 
 //cubie move vectors
 //automate assignment?
-uint8_t* (*edge_perm[18])(uint8_t[12]);
-uint8_t* (*edge_ori[18])(uint8_t[12]);
-uint8_t* (*corner_perm[18])(uint8_t[8], int);
-uint8_t* (*corner_ori[18])(uint8_t[8], int);
+typedef uint8_t* (*edgePtr)(uint8_t*);
+typedef uint8_t* (*cornPtr)(uint8_t*, int);
+
+edgePtr *edge_perm;
+edgePtr *edge_ori;
+cornPtr *corner_perm;
+cornPtr *corner_ori;
 
 //move tables
 uint32_t *ep_table;
@@ -16,7 +19,7 @@ uint16_t *eo_table;
 uint16_t *cp_table;
 uint16_t *co_table;
 
-void make_ep_table(uint8_t* (*edge_perm[18])(uint8_t[12]), int moves_length) {
+void make_ep_table(edgePtr *ep, int moves_length) {
     uint8_t edges_p[12];
     uint8_t edges_p_2[12];
     uint64_t temp = 0;
@@ -27,9 +30,10 @@ void make_ep_table(uint8_t* (*edge_perm[18])(uint8_t[12]), int moves_length) {
     for(uint64_t i = 0; i < 665280; i++){
         std::copy(&edges_p[0], &edges_p[12], &edges_p_2[0]);
         temp = 6*encode_perm(6, 12, edges_p);
+        //std::cout << "edge temp: " << temp << std::endl;
         for(int j = 0; j < moves_length; j++){
             std::copy(&edges_p_2[0], &edges_p_2[12], &edges_p[0]);
-            ep_table[temp+j] = encode_perm(6, 12, edge_perm[j](edges_p));
+            ep_table[temp+j] = encode_perm(6, 12, ep[j](edges_p));
         }
         for (int j = 0; j < 12; j++){
             if (edges_p[j] > 5){
@@ -44,12 +48,13 @@ void make_ep_table(uint8_t* (*edge_perm[18])(uint8_t[12]), int moves_length) {
             iteration++;
         }
     }
-    FILE *f;
+    /*FILE *f;
     f = fopen("ep_moves.table", "wb");
-    fwrite(ep_table, 4, 3991680, f);
+    fwrite(ep_table, 4, 665280*moves_length, f);*/
 }
 
-void make_cp_table(uint8_t* (*corner_perm[18])(uint8_t[8], int), int moves_length) {
+void make_cp_table(cornPtr *cp, int moves_length) {
+    std::cout << "hello" << std::endl;
     uint8_t corners_p[8];
     uint8_t corners_p_2[8];
     uint64_t temp = 0;
@@ -59,18 +64,23 @@ void make_cp_table(uint8_t* (*corner_perm[18])(uint8_t[8], int), int moves_lengt
     for(uint64_t i = 0; i < 40320; i++){
         std::copy(&corners_p[0], &corners_p[8], &corners_p_2[0]);
         temp = 6*encode_perm(8, corners_p);
+        for (int j = 0; j < 8; j++){
+            std::cout << (int)corners_p[j] << " ";
+        }
+        std::cout << "corner temp: " << temp << std::endl;
         for(int j = 0; j < moves_length; j++){
             std::copy(&corners_p_2[0], &corners_p_2[8], &corners_p[0]);
-            cp_table[temp+j] = encode_perm(8, corner_perm[j](corners_p, 1));
+            cp_table[temp+j] = encode_perm(8, cp[j](corners_p, 1));
         }
         std::next_permutation(&corners_p[0], &corners_p[8]);
     }
+    std::cout << "hi" << std::endl;
     FILE *f;
     f = fopen("cp_moves.table", "wb");
-    fwrite(cp_table, 2, 241920, f);
+    fwrite(cp_table, 2, 40320*moves_length, f);
 }
 
-void make_eo_table(uint8_t* (*edge_ori[18])(uint8_t[12]), int moves_length) {
+void make_eo_table(edgePtr *eo, int moves_length) {
     uint8_t *edges_o = new uint8_t[12];
     uint8_t *edges_o_2 = new uint8_t[12];
     for(uint64_t i = 0; i < 12; i++){
@@ -78,17 +88,17 @@ void make_eo_table(uint8_t* (*edge_ori[18])(uint8_t[12]), int moves_length) {
         std::copy(&edges_o[0], &edges_o[12], &edges_o_2[0]);
         for(int j = 0; j < moves_length; j++){
             std::copy(&edges_o_2[0], &edges_o_2[12], &edges_o[0]);
-            eo_table[6*i] = encode_ori(2, 12, edge_ori[j](edges_o));
+            eo_table[6*i] = encode_ori(2, 12, eo[j](edges_o));
         }
     }
     delete edges_o;
     delete edges_o_2;
     FILE *f;
     f = fopen("eo_moves.table", "wb");
-    fwrite(eo_table, 2, 12288, f);
+    fwrite(eo_table, 2, 2048*moves_length, f);
 }
 
-void make_co_table(uint8_t* (*corner_ori[18])(uint8_t[8], int), int moves_length) {
+void make_co_table(cornPtr *co, int moves_length) {
     uint8_t *corners_o = new uint8_t[8];
     uint8_t *corners_o_2 = new uint8_t[8];
     for(uint64_t i = 0; i < 12; i++){
@@ -96,23 +106,33 @@ void make_co_table(uint8_t* (*corner_ori[18])(uint8_t[8], int), int moves_length
         corners_o = decode_ori(3, 8, i);
         for(int j = 0; j < moves_length; j++){
             std::copy(&corners_o_2[0], &corners_o_2[12], &corners_o[0]);
-            co_table[6*i] = encode_ori(3, 8, corner_ori[j](corners_o, 1));
+            co_table[6*i] = encode_ori(3, 8, co[j](corners_o, 1));
         }
     }
     delete corners_o;
     delete corners_o_2;
     FILE *f;
     f = fopen("co_moves.table", "wb");
-    fwrite(co_table, 2, 13122, f);
+    fwrite(co_table, 2, 2187*moves_length, f);
 }
 
 void make_move_tables(int moves, int moves_length){
+    edge_perm = new edgePtr[6];
+    edge_ori = new edgePtr[6];
+    corner_perm = new cornPtr[6];
+    corner_ori = new cornPtr[6];
     edge_perm[0] = &move_U_perm;
     edge_perm[1] = &move_D_perm;
     edge_perm[2] = &move_R_perm;
     edge_perm[3] = &move_L_perm;
     edge_perm[4] = &move_F_perm;
     edge_perm[5] = &move_B_perm;
+    corner_perm[0] = &move_U_perm;
+    corner_perm[1] = &move_D_perm;
+    corner_perm[2] = &move_R_perm;
+    corner_perm[3] = &move_L_perm;
+    corner_perm[4] = &move_F_perm;
+    corner_perm[5] = &move_B_perm;
     corner_ori[0] = &move_U_ori;
     corner_ori[1] = &move_D_ori;
     corner_ori[2] = &move_R_ori;

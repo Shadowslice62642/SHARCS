06eccb2cb3f9cbe44d470d4ec4f3b1d60323fe1a
Joseph Briggs <joseph.briggs@live.co.uk>
2021-09-18 16:14:28 +0100

    added step

---
+++ solver.h
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "coordMoves.h"
+
+typedef struct step {
+    uint32_t *ep_moves;
+    uint16_t *cp_moves;
+    uint16_t *eo_moves;
+    uint16_t *co_moves;
+    char *prune1;
+    char *prune2;
+    char *prune3;
+    char *prune4;
+};

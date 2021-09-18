f5508c19116712ab7989b36abdb98a2e1cb8c0fc
Joseph Briggs <joseph.briggs@live.co.uk>
2021-09-18 19:50:33 +0100

    added #pragma once

--- cubieMoves.h
+++ cubieMoves.h
@@ -1,3 +1,4 @@
+#pragma once
 #include <iostream>
 
 //corners_p
--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include "coordMoves.h"
+#include "solver.h"
+#include "cubieMoves.h"
 
 int main(){
     make_move_tables(6, 6);
